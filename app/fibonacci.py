822104de2b7bd85c8e028b3bcc04aab7a9c108ad
vivy_phtela <tubasa.0405@icloud.com>
2024-07-08 23:36:31 +0900

    再帰の廃止

--- app/fibonacci.py
+++ app/fibonacci.py
@@ -17,4 +17,7 @@ def calculate_fibonacci(n: int) -> int:
     """
     if n == 1 or n == 2:
         return 1
-    return calculate_fibonacci(n - 1) + calculate_fibonacci(n - 2)
+    previous, current = 1, 1
+    for _ in range(2, n):
+        previous, current = current, previous + current
+    return current
