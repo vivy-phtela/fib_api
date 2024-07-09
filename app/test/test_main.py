49c8abf280309ffe8b12ec2a69d48e2f07923608
vivy_phtela <tubasa.0405@icloud.com>
2024-07-09 16:42:05 +0900

    テストケースの追加

--- app/test/test_main.py
+++ app/test/test_main.py
@@ -13,6 +13,10 @@ def test_fibonacci_99(): # n=99のとき
     assert response.status_code == 200
     assert response.json() == {"result": 218922995834555169026}
 
+def test_zero(): # nが0のとき
+    response = client.get("/fib?n=0")
+    assert response.status_code == 422
+
 def test_negative(): # nが負の数のとき
     response = client.get("/fib?n=-1")
     assert response.status_code == 422
@@ -23,4 +27,8 @@ def test_decimal(): # nが小数のとき
 
 def test_string(): # nが文字列のとき
     response = client.get("/fib?n=abc")
+    assert response.status_code == 422
+
+def test_empty(): # nが指定されていないとき
+    response = client.get("/fib?n=")
     assert response.status_code == 422
